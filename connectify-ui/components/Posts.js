bb32076adcf9e9daf74ab96b8efa339ae7b853d1
Sriranjan <sriranjan.s@brillio.com>
2024-07-02 00:19:59 -0700

    Updated Development Documentation

--- connectify-ui/components/Posts.js
+++ connectify-ui/components/Posts.js
@@ -1,26 +1,25 @@
-import React, { useEffect } from 'react';
-import Post from './Post';
-import { useDispatch, useSelector } from 'react-redux';
-import { addAllPost, selectPost } from '../public/src/features/postSlice';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import Post from "./Post";
+import { useDispatch, useSelector } from "react-redux";
+import { addAllPost, selectPost } from "../public/src/features/postSlice";
+import axios from "axios";
 
 const Posts = () => {
-  const CONNECTIFY_ENDPOINT = "";
+  const CONNECTIFY_ENDPOINT = "http://localhost:8080/api/v1/post";
 
   const dispatch = useDispatch();
   const posts = useSelector(selectPost);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get("http://localhost:8080/api/v1/post")
-        .then((response) => {
-          console.log(response.data);
-          dispatch(addAllPost(response.data));
-        });
+    const fetchData = () => {
+      const response = axios.get(CONNECTIFY_ENDPOINT).then((response) => {
+        console.log(response.data);
+        dispatch(addAllPost(response.data));
+      });
     };
     fetchData();
     console.log(posts);
-  }, [dispatch]);
+  }, []);
 
   return (
     <div>
